00b9d80bcaec35bdea0c79117c430914a9c333a3
wesam ahmed <wesamahmed88.5@gmail.com>
2020-12-16 23:33:03 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_shop/Counters/BookQuantity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'Authentication/authenication.dart';
+import 'package:e_shop/Config/config.dart';
+import 'Counters/cartitemcounter.dart';
+import 'Counters/changeAddresss.dart';
+import 'Counters/totalMoney.dart';
+import 'Store/storehome.dart';
+
+Future<void> main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  EcommerceApp.auth=FirebaseAuth.instance;
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+            title: 'e-Shop',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primaryColor: Colors.green,
+            ),
+            home: SplashScreen()
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+
+class _SplashScreenState extends State<SplashScreen>
+{
+  @override
+  void initState() {
+    // TODO: implement initState
+    displaySplash();
+  }
+  displaySplash(){
+    Timer(Duration(seconds: 5), ()async{
+      if(await EcommerceApp.auth.currentUser() !=null){
+        Route route=MaterialPageRoute(builder: (_) => StoreHome());
+        Navigator.pushReplacement(context, route);
+      }else{
+        Route route=MaterialPageRoute(builder: (_) => AuthenticScreen());
+        Navigator.pushReplacement(context, route);
+      }
+
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(colors:[Colors.white,Colors.grey],
+          begin:const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0,1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/welcome.png"),
+              SizedBox(height: 20.0,),
+              Text("welcome to lbsny",style: TextStyle(color: Colors.black54),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
