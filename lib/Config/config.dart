da6d69275139c726d0a1bd7252ecdcb187881892
Osama <mo7amedos236@gmail.com>
2021-05-18 00:45:11 +0200

    Change App Name & Fix Bugs (Osama)

--- lib/Config/config.dart
+++ lib/Config/config.dart
@@ -5,7 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class EcommerceApp
 {
-   static const String appName = 'e-Shop';
+   static const String appName = 'E-CLOTHES';
 
    static SharedPreferences sharedPreferences;
    static FirebaseUser user;
